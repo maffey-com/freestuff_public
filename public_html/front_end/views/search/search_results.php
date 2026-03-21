39803982c8e259e73918a2353cd4af9fabe5dd53
Cameron Belcher <neteree@gmail.com>
2026-03-21 16:56:45 +1300

    fix: correct h() double echo in top givers list

--- public_html/front_end/views/search/search_results.php
+++ public_html/front_end/views/search/search_results.php
@@ -68,13 +68,13 @@
                 <h4>Top Givers</h4>
                 <ul>
                     <? foreach ($top_givers as $giver) { ?>
-                        <li><?= h($giver['user_firstname']) ?>: <?= $giver['given_count'] ?> <?= $giver['given_count'] == 1 ? 'item' : 'items' ?> given away</li>
+                        <li><? h($giver['user_firstname']) ?>: <?= $giver['given_count'] ?> <?= $giver['given_count'] == 1 ? 'item' : 'items' ?> given away</li>
                     <? } ?>
                 </ul>
             </div>
         </div>  
     <? } 
-    
+
     require('templates/common_pager_bottom.php');
     ?>
 </div>
