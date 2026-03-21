e77541ad37b23f3fe1cdfeb2387598c830dfab2c
Cameron Belcher <neteree@gmail.com>
2026-03-21 20:09:27 +1300

    fix: update modal messaging when all requesters are no-shows

--- public_html/front_end/views/list/mark_as_gone_modal.php
+++ public_html/front_end/views/list/mark_as_gone_modal.php
@@ -4,21 +4,30 @@
         <h5 class="modal-title">Did anyone take this item?</h5>
     </div>
     <div class="modal-body">
-        <? if (count($requests) == 0) { ?>
-            <p>No one has requested this item yet.</p>
-        <? } else { ?>
-            <p>Selecting a user will also give them a positive feedback</p>
-        <? }
-        foreach ($requests as $request) {
-            if ($request['no_show'] === 'y') continue;
-            ?>
-                <p>
-                    <span class="btn btn-link mark-item-taken" data-user_id="<?= $request['user_id'] ?>">
-                        <b><?= $request['user_firstname'] ?></b> of <b><?= $request['district'] ?></b>
-                    </span>
-                </p>
-            <?
-        } ?>
+        <? 
+            $available_requests = array_filter($requests, fn($r) => $r['no_show'] !== 'y');
+
+            if (count($available_requests) == 0) {
+                ?>
+                    <p>All requesters have been marked as no shows.</p>
+                <? 
+            } else {
+                ?>
+                    <p>Selecting a user will also give them a positive feedback</p>
+                <?
+            }
+
+            foreach ($requests as $request) {
+                if ($request['no_show'] === 'y') continue;
+                ?>
+                    <p>
+                        <span class="btn btn-link mark-item-taken" data-user_id="<?= $request['user_id'] ?>">
+                            <b><?= $request['user_firstname'] ?></b> of <b><?= $request['district'] ?></b>
+                        </span>
+                    </p>
+                <?
+            }
+        ?>
     </div>
     <div class="modal-footer">
         <span class="btn btn-secondary mark-item-taken"
